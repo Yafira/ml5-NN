c099ff962251f0bd92452a685a12c0cfa11d3e3e
Yafira <yafira.martinez@lc.cuny.edu>
2020-08-04 19:22:48 -0400

    collecting training data through user interactions

--- NN/sketch.js
+++ NN/sketch.js
@@ -1,14 +1,30 @@
 let model;
+let targetLabel = 'C';
 
 function setup() {
-createCanvas(400, 400);
+    createCanvas(400, 400);
 
-let options = {
-
-};
-model = ml5.neuralNetwork(options);
+    let options = {
+        inputs: ['x', 'y'],
+        outputs: ['label'],
+        task: 'classification'
+    };
+    model = ml5.neuralNetwork(options);
+    background(255);
 }
 
-function draw() {
-    background(255);
+function keyPressed() {
+    targetLabel = key.toUpperCase();
+}
+
+// collect training data
+function mousePressed() {
+
+    stroke(0);
+    noFill();
+    ellipse(mouseX, mouseY, 24);
+    fill(0);
+    noStroke();
+    textAlign(CENTER, CENTER);
+    text(targetLabel, mouseX, mouseY);
 }
